c1d4a845682aca9632c094ab27ce8bbdc0e8130e
Iurii <boiko.iurii86@gmail.com>
2020-01-21 00:35:05 +0200

    unit_6

---
+++ unit_6/myapp/src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="section footer-classic context-dark bg-image" style={{ background: "#d2d346", color: "fff" }}>
+                <p>Copyright</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
