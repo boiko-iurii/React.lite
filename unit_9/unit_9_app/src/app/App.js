d657fef9bf34f0c071bf82d754dd222cf8852336
Iurii <boiko.iurii86@gmail.com>
2020-01-26 15:47:56 +0200

    finished unit_9

--- unit_9/unit_9_app/src/app/App.js
+++ unit_9/unit_9_app/src/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+const img = 'https://cdn.evilmartians.com/front/posts/optimizing-react-virtual-dom-explained/cover-a1d5b40.png';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +9,12 @@ class App extends React.Component {
       text: "",
       count: 0,
       input: "",
-      warning: ""
+      warning: "",
+      radio: "",
+      select: "",
+      width: 150,
+      range: 0,
+      toggle: false
     }
     this.task2 = this.task2.bind(this);
     this.addCount = this.addCount.bind(this);
@@ -23,10 +29,8 @@ class App extends React.Component {
     })
   }
   addCount() {
-    let i = this.state.count;
-    i++;
     this.setState({
-      count: i
+      count: this.state.count + 1
     });
   }
   resetCount() {
@@ -56,7 +60,36 @@ class App extends React.Component {
   }
   func9 = (event) => {
     if (event.target.checked) console.log('checked')
-    else console.log('unchecked')
+    else console.log('unchecked');
+  }
+  func10 = (event) => {
+    this.setState({
+      radio: event.target.value
+    })
+  }
+  func11 = (event) => {
+    this.setState({
+      select: event.target.value
+    });
+  }
+  addWidth = () => {
+    this.setState({
+      width: this.state.width + 3
+    });
+  }
+  resetWidth = () => {
+    this.setState({
+      width: 150
+    });
+  }
+  changeRange = (event) => {
+    this.setState({
+      range: event.target.value
+    })
+  }
+  toggleImage = () => {
+    const currentState = this.state.toggle;
+    this.setState({ toggle: !currentState });
   }
   render() {
     return (
@@ -84,14 +117,35 @@ class App extends React.Component {
           <h4>Task 9</h4>
           <label htmlFor="ch9"><input type="checkbox" onChange={this.func9} id="ch9" />Checkbox</label>
           <h4>Task 10</h4>
-          <label htmlFor="rb1"><input type="radio" name="rbt10" onChange={this.func9} id="rb1" />radio1</label>
-          <label htmlFor="rb2"><input type="radio" name="rbt10" onChange={this.func9} id="rb2" />radio1</label>
-          <label htmlFor="rb3"><input type="radio" name="rbt10" onChange={this.func9} id="rb3" />radio1</label>
-
+          <label htmlFor="rb1"><input type="radio" name="rbt10" onChange={this.func10} id="rb1" value="r1" />radio1</label>
+          <label htmlFor="rb2"><input type="radio" name="rbt10" onChange={this.func10} id="rb2" value="r2" />radio2</label>
+          <label htmlFor="rb3"><input type="radio" name="rbt10" onChange={this.func10} id="rb3" value="r3" />radio3</label>
+          <p>{this.state.radio}</p>
           <h4>Task 11</h4>
+          <select onChange={this.func11}>
+            <option value=""></option>
+            <option value="o1">1</option>
+            <option value="o2">2</option>
+            <option value="o3">3</option>
+            <option value="o4">4</option>
+            <option value="o5">5</option>
+            <option value="o6">6</option>
+          </select>
+          <p>{this.state.select}</p>
           <h4>Task 12</h4>
+          <button onClick={this.addWidth}>addWidth</button>
+          <button onClick={this.resetWidth}>resetWidth</button>
+          <div className="div12" style={{ width: this.state.width }}></div>
           <h4>Task 13</h4>
+          <input type="range" id="r13" min="0" max="100" step="1" onChange={this.changeRange} value={this.state.range} />
+          <p>{this.state.range}</p>
           <h4>Task 14</h4>
+          <button onClick={this.toggleImage}>click</button>
+          <img src={img} alt="" id="img" style={{
+            width: "50%",
+            height: "auto",
+            display: this.state.toggle ? 'none' : 'block'
+          }} />
         </div>
       </div >
     );
