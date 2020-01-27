d22cac627e2f2ebee6ecba4a7c8a668bdb923ab8
Iurii <boiko.iurii86@gmail.com>
2020-01-28 00:42:01 +0200

    unit_10

--- unit_10/unit_10_app/src/app/App.js
+++ unit_10/unit_10_app/src/app/App.js
@@ -16,38 +16,70 @@ class App extends React.Component {
   }
 
   handleField = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   submitForm = (e) => {
     e.preventDefault();
+    let out =
+      <table>
+        <tbody><tr>
+          <th>Name</th>
+          <th>Значение</th>
+        </tr>
+          <tr>
+            <td>text</td>
+            <td>{this.state.text}</td>
+          </tr>
+          <tr>
+            <td>number</td>
+            <td>{this.state.number}</td>
+          </tr>
+          <tr>
+            <td>range</td>
+            <td>{this.state.range}</td>
+          </tr>
+          <tr>
+            <td>textarea</td>
+            <td>{this.state.textarea}</td>
+          </tr>
+          <tr>
+            <td>select</td>
+            <td>{this.state.select}</td>
+          </tr>
+        </tbody>
+      </table>;
+
     this.setState({
-      text: e.target.text.value,
-      number: e.target.number.value
+      out: out
     });
 
-    console.log(e.target.number.name);
-    console.log('submited');
   }
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.submitForm}>
-          <div><input type="text" name="text" /></div>
-          {/* <div><input type="number" name="number" onChange={this.handleField} /></div> */}
-          <div><input type="number" name="number" /></div>
-
-          <input type="submit" className="button-primary" />
-        </form>
-        <div className="table">
-          <div className="table-head">
-            <div>name</div><div>значение</div>
+        <form onSubmit={this.submitForm} id="form">
+          <div><input type="text" name="text" onChange={this.handleField} /></div>
+          <div><input type="number" name="number" onChange={this.handleField} /></div>
+          <div><input type="range" name="range" min="0" max="21" step="1" value={this.state.range} onChange={this.handleField} /></div>
+          <div><textarea rows="5" name="textarea" onChange={this.handleField} /></div>
+          <div>
+            <select name="select" onChange={this.handleField} >
+              <option value=""></option>
+              <option value="01">1</option>
+              <option value="02">2</option>
+              <option value="03">3</option>
+              <option value="04">4</option>
+              <option value="05">5</option>
+            </select>
           </div>
-          {/* <div className="table-row">
-            <div>{this.state.text}</div><div>{this.state.text}</div>
-          </div> */}
-          {this.state.text}<br />
-          {this.state.number}<br />
-        </div>
+          <input type="submit" className="button-primary" />
+
+          {this.state.out}
+
+        </form>
       </div>
     );
   }
