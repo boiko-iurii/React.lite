f29ac4454f57cb5dafd710aee87c12cb91a2e7a4
Boiko Iurii <boiko.iurii86@gmail.com>
2020-01-27 18:12:05 +0200

    unit_10

--- unit_10/unit_10_app/src/app/App.js
+++ unit_10/unit_10_app/src/app/App.js
@@ -10,46 +10,43 @@ class App extends React.Component {
       number: 0,
       range: 0,
       textarea: '',
-      select: ''
+      select: '',
+      out: ''
     }
   }
+
+  handleField = (e) => {
+  }
+
   submitForm = (e) => {
     e.preventDefault();
     this.setState({
-      [e.target.name]: e.target.value
+      text: e.target.text.value,
+      number: e.target.number.value
     });
-    console.log(e.target.text.name);
-    console.log(e.target.text.value);
-  }
-  handleField = (e) => {
-    console.log(e.target.value)
+
+    console.log(e.target.number.name);
+    console.log('submited');
   }
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.submitForm} id="form">
-          <div><input type="text" name="text" onChange={this.handleField} /></div>
-          <div><input type="number" name="number" onChange={this.handleField} /></div>
-          <div><input type="range" name="range" onChange={this.handleField} /></div>
-          <div><textarea rows="5" name="textarea" onChange={this.handleField} /></div>
-          <div>
-            <select name="select" >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </div>
+        <form onSubmit={this.submitForm}>
+          <div><input type="text" name="text" /></div>
+          {/* <div><input type="number" name="number" onChange={this.handleField} /></div> */}
+          <div><input type="number" name="number" /></div>
+
           <input type="submit" className="button-primary" />
         </form>
         <div className="table">
           <div className="table-head">
             <div>name</div><div>значение</div>
           </div>
-          <div className="table-row">
+          {/* <div className="table-row">
             <div>{this.state.text}</div><div>{this.state.text}</div>
-          </div>
+          </div> */}
+          {this.state.text}<br />
+          {this.state.number}<br />
         </div>
       </div>
     );
