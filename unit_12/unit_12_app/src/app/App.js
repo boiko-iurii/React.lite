7b1c513c7ae51a69da43ed2e521edd4ba52813ed
Iurii <boiko.iurii86@gmail.com>
2020-01-31 01:15:12 +0200

    unit_12

--- unit_12/unit_12_app/src/app/App.js
+++ unit_12/unit_12_app/src/app/App.js
@@ -6,7 +6,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      count: 0
+      count: 0,
+      countX: 0,
+      countO: 0,
+      sign: 0
     }
     this.winnerLine = [
       [0, 1, 2],
@@ -18,24 +21,32 @@ class App extends React.Component {
       [0, 4, 8],
       [2, 4, 6]
     ]
+    this.winner = false;
+
   }
   isWinner = () => {
     let s = (this.state.count % 2 === 0) ? 'X' : 'O';
+    let currentCountX = (s === 'X') ? 1 : 0;
+    let currentCountO = (s === 'O') ? 1 : 0;
+
     for (let i = 0; i < 8; i++) {
       let line = this.winnerLine[i];
-      // console.log(this.state.squares[line[0]]);
       if (this.state.squares[line[0]] === s
         && this.state.squares[line[1]] === s
         && this.state.squares[line[2]] === s) {
-        console.log(s + ' winner');
-        setTimeout(() => {
-          this.setState({
-            squares: Array(9).fill(null),
-            count: 0
-          })
-        }, 2000);
+        this.winner = true;
+        this.setState({
+          countX: this.state.countX + currentCountX,
+          countO: this.state.countO + currentCountO
+        })
+        alert(s + ' winner');
       }
     }
+  }
+  isDraw = () => {
+    if (this.state.squares.indexOf(null) === -1
+      && this.winner === false)
+      alert('is draw');
   }
   clickHandler = event => {
     let data = event.target.getAttribute('data');
@@ -48,9 +59,23 @@ class App extends React.Component {
       });
     }
     this.isWinner();
+    this.isDraw();
+  }
+  newGame = () => {
+    this.setState({
+      squares: Array(9).fill(null),
+      count: this.state.sign
+    })
+    this.winner = false;
+  }
+  firstSign = (event) => {
+    let sign = +event.target.getAttribute('data-sign');
+    this.setState({
+      sign: sign,
+      count: sign
+    });
   }
   render() {
-
     return (
       <div className="tic-tac-toe">
         <div className="ttt-grid" onClick={this.clickHandler} data="0">{this.state.squares[0]}</div>
@@ -62,6 +87,16 @@ class App extends React.Component {
         <div className="ttt-grid" onClick={this.clickHandler} data="6">{this.state.squares[6]}</div>
         <div className="ttt-grid" onClick={this.clickHandler} data="7">{this.state.squares[7]}</div>
         <div className="ttt-grid" onClick={this.clickHandler} data="8">{this.state.squares[8]}</div>
+        <button className="new-game" onClick={this.newGame}>Новая игра</button>
+        <div className="score">
+          <h5>Количество побед</h5>
+          <div>X:<b>{this.state.countX}</b></div><div>O:<b>{this.state.countO}</b></div>
+        </div>
+        <div className="first-sign">
+          <h5>Право первого хода:</h5>
+          <button className="first-sign-btn" data-sign="0" onClick={this.firstSign}>X</button>
+          <button className="first-sign-btn" data-sign="1" onClick={this.firstSign}>O</button>
+        </div>
       </div>
     );
   }
